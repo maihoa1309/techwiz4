832ad27704bd6b437abad2afcf37ef0a237e96fa
tho <tho@gmail.com>
2023-08-09 22:44:26 +0700

    duoc roi hehe

--- Models/Subscription.cs
+++ Models/Subscription.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StreamTrace.Models
 {
-    public class Subscription:Base
+    public class Subscription : Base
     {
-   
-        
+
+
         public int? ServiceId { get; set; }
         [ForeignKey("ServiceId")]
         public Service? GetService { get; set; }
