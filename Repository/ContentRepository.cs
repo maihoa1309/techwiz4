5de4260a4c54f511a9304ea8806ccee8657516f6
Mai Hoa <hoa.btm1309@gmail.com>
2023-08-10 08:34:16 +0700

    update repository

--- Repository/ContentRepository.cs
+++ Repository/ContentRepository.cs
@@ -6,11 +6,20 @@ namespace StreamTrace.Repository
 {
     public interface IContentRepository : IBaseRepository<Content>
     {
-
+        Task<List<Content>> GetContentByName(string name, int index, int size);
 
     }
     public class ContentRepository : BaseRepository<Content>, IContentRepository
     {
         public ContentRepository(ApplicationDbContext dbContext, UserManager<CustomUser> userManager, IHttpContextAccessor httpContext): base(dbContext, userManager, httpContext) { }
+
+        public async Task<List<Content>> GetContentByName(string name, int index, int size)
+        {
+            var result = await FilterAsync( r => r.GetService,
+                                            r => r.Name.ToLower().Contains(name.ToLower()) || r.GetService.Name.ToLower().Contains(name.ToLower()), 
+                                            index: index,
+                                            size: size);
+            return result;
+        }
     }
 }
